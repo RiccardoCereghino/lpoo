647ea425e5e4a61b23d539fda89d6fbd66300e83
Riccardo Cereghino <riccardo.cereghino@gmail.com>
2019-12-05 08:53:03 +0100

    Labo

---
+++ labs/lab-x/Bool.java
@@ -0,0 +1,26 @@
+/**
+ * Bool
+ */
+
+public class Bool {
+  static final boolean FALSE = false;
+  static final boolean TRUE = true;
+
+  public
+
+  static Bool valueOf(boolean b){
+    return b ? new Bool(true) : ;
+  }
+
+  boolean boolValue(){
+    return true;
+  }
+
+  /**
+  boolean equals(Object obj){
+
+  }
+  */
+}
+
+
